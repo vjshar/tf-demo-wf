c61abd053ba30ac3e379a631c3159d9325c33c93
vjshar <vijay.sharma@hashicorp.com>
2024-05-20 16:25:53 -0400

    Remove cloud block

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+/*
+  cloud {
+    organization = "TFC-Dev-Env"
+
+    workspaces {
+      name = "tf-demo-env"
+    }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
